2d1db1156537fb943385392a1cc6cdc31906b54d
Payam Jalili <payamjalili@users.noreply.github.com>
2021-11-26 20:37:04 +0100

    Build: Digibook

---
+++ source/redux/Reducers/globalReducer.js
@@ -0,0 +1,9 @@
+export default (state={},action={})=>
+                        {return {baseApiUrl:'https://digibook.iran.liara.run/wp-json/v1',
+                        grColorOne:'#38c3fa',
+                        grColorTwo:'#0590c7',
+                        newDate:new Date().toISOString(),
+                        website:'https://digibook.iran.liara.run/',
+                        appName:'دیجی بوک',
+                        slogan:'یک اتفاق تازه',
+                        };};
